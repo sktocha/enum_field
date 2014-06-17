4fa6f19d04651e7d576e2583465f4c8a63540396
Igor Galeta <galeta.igor@gmail.com>
2014-06-17 16:49:46 +0300

    Add param class_name

--- lib/enum_field/enumerated_attribute.rb
+++ lib/enum_field/enumerated_attribute.rb
@@ -36,14 +36,16 @@ module EnumField
     #      +name_attribute+ with an +_id+ suffix.
     #    * +class+: the class that will be instantiated when +name_attribute+ method is called. Defaults to
     #      +name_attribute+ in camelcase form.
+    #
     def enumerated_attribute(name_attribute, options  = {})
-      id_attribute = options[:id_attribute] || (name_attribute.to_s + '_id').to_sym
-      klass = options[:class] || name_attribute.to_s.camelcase.constantize
+      id_attribute = (options[:id_attribute] || "#{name_attribute}_id").to_sym
+      klass = options[:class] || (options[:class_name] || name_attribute).to_s.camelcase.constantize
+
       define_method(name_attribute) do
         (raw = read_attribute(id_attribute)) && klass.find_by_id(raw)
       end
 
-      define_method(name_attribute.to_s + '=') do |value|
+      define_method("#{name_attribute}=") do |value|
         write_attribute(id_attribute, value ? value.id : nil)
       end
     end
